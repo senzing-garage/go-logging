8d438e25ee2f6cb7631dc3393b5c37482877b0d8
docktermj <michael@dockter.com>
2022-11-04 16:24:11 -0400

    #20 Savepoint

--- main_test.go
+++ main_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"testing"
 
+	"github.com/senzing/go-logging/logger"
 	"github.com/senzing/go-logging/messagelogger"
 )
 
@@ -27,6 +28,24 @@ func TestNewSenzingLogger(t *testing.T) {
 	}
 
 	logger, _ := messagelogger.NewSenzingLogger(productIdentifier, idMessages)
+	logger.Log(1, "Mary")
+	logger.Log(1000, "Jane")
+	logger.Log(2000, "Bob")
+}
+
+func TestNewSenzingLoggerWithOnlyError(t *testing.T) {
+
+	// func NewSenzingLogger(productIdentifier int, idMessages map[int]string, interfaces ...interface{}) (MessageLoggerInterface, error) {
+
+	productIdentifier := 9999
+
+	idMessages := map[int]string{
+		0001: "Info for %s",
+		1000: "Warning for %s",
+		2000: "Error for %s",
+	}
+
+	logger, _ := messagelogger.NewSenzingLogger(productIdentifier, idMessages, logger.LevelError)
 	logger.Log(1, "Mary")
 	logger.Log(1000, "Jane")
 	logger.Log(2000, "Bob")
